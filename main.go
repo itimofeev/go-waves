858f8b5f504dc8d992e7a697a47026957bfee198
Ilya Timofeev <ilya.ufo@gmail.com>
2017-10-09 08:03:20 +0300

    Generate keys

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"github.com/btcsuite/btcutil/base58"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/wsddn/go-ecdh"
 	"golang.org/x/crypto/blake2b"
+	"crypto/sha256"
 )
 
 const seedString = "manage manual recall harvest series desert melt police rose hollow moral pledge kitten position add"
@@ -19,6 +22,15 @@ func main() {
 	accountSeed := makeSeedHash(0, seedBytes)
 	fmt.Println("account seed with nonce 0", encodeBase58(accountSeed))
 
+	hashedAccountSeed := sha256.Sum256(accountSeed)
+	fmt.Println("hashed account seed ", encodeBase58(hashedAccountSeed[:]))
+
+	g := ecdh.NewCurve25519ECDH()
+	priv, pub, err := g.GenerateKey(bytes.NewReader(hashedAccountSeed[:]))
+	must(err)
+
+	fmt.Println("Private key", encodeBase58(g.Marshal(priv)))
+	fmt.Println("Public key", encodeBase58(g.Marshal((pub))))
 }
 
 func encodeBase58(src []byte) string {
@@ -33,9 +45,12 @@ func prependNonce(nonce uint32, seedBytes []byte) []byte {
 }
 
 func makeSeedHash(nonce uint32, seedBytes []byte) []byte {
-	blake2bSum := make([]byte, 32)
 	sum := blake2b.Sum256(prependNonce(nonce, seedBytes))
-	copy(blake2bSum, sum[:])
+	return crypto.Keccak256(sum[:])
+}
 
-	return crypto.Keccak256(blake2bSum)
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
